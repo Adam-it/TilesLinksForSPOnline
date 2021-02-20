c0b78737f3330b638cdcafb794a2542906424ceb
Adam <58668583+Adam-it@users.noreply.github.com>
2021-02-20 23:06:53 +0100

    rebuil loader based on fluent UI spinner and removed the custom one (#11)

--- Webpart/src/webparts/personalTiles/components/loader/Loader.tsx
+++ Webpart/src/webparts/personalTiles/components/loader/Loader.tsx
@@ -3,13 +3,13 @@ import * as strings from 'PersonalTilesWebPartStrings';
 import ILoaderProps from './ILoaderProps';
 import ILoaderState from './ILoaderState';
 import mainStyles from '../../styles/PersonalTiles.module.scss';
-import { Icon } from 'office-ui-fabric-react/lib/Icon';
+import { Spinner } from 'office-ui-fabric-react/lib/Spinner';
 
 export default class Loader extends React.Component<ILoaderProps, ILoaderState> {
   public render() {
     return (
       <div className={mainStyles.loader}>
-          <label><Icon iconName={"ProgressRingDots"} /> {strings.Loading}</label>
+        <Spinner label={strings.Loading} ariaLive="assertive" labelPosition="right" />
       </div>
     );
   }
