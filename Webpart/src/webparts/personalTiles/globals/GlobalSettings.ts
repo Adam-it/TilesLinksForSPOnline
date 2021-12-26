95d26255609fd3566c00cc268f5c95639bb839a8
Adam <adam.wojcik.it@gmail.com>
2021-12-26 18:32:14 +0100

    code cleanup

--- Webpart/src/webparts/personalTiles/globals/GlobalSettings.ts
+++ Webpart/src/webparts/personalTiles/globals/GlobalSettings.ts
@@ -3,7 +3,7 @@ export default class GlobalSettings {
     public static appDataFolderName: string = 'PersonalTilesApp';
     public static appDataJsonFileName: string = 'PersonalTilesData.json';
     public static httpsProtocol: string = 'https://';
-    public static adminSite: string = 'https://tenanttocheck.sharepoint.com/sites/PersonalLinksAdminSite5';
+    public static adminSite: string = 'https://sample.sharepoint.com/sites/PersonalLinksAdminSite';
     public static predifinedList: string = 'Predefined Personal Links';
     public static linkName: string = 'Link_x0020_Name';
     public static linkUrl: string = 'Link_x0020_Url';
